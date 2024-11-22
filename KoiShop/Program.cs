e0d6275f2dccd7a4f66d765030a4adcc9ab2c91d
NotGiaBao <131367528+Baooooooo0@users.noreply.github.com>
2024-11-23 00:12:34 +0700

    First Commit

---
+++ KoiShop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2251120269 - Lam Dang Gia Bao\n============Bai tap 1============\n");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
