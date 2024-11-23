d4951a8f6dbe03da4d32beb32de5e5d3d391092c
ngtr_bl <166531592+ngtrbaolong@users.noreply.github.com>
2024-11-23 12:06:40 +0700

    Update KoiFishApplication.java

--- KoiShop/KoiFish/src/main/java/com/example/koifish/KoiFishApplication.java
+++ KoiShop/KoiFish/src/main/java/com/example/koifish/KoiFishApplication.java
@@ -2,7 +2,7 @@ package com.example.koifish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//SpringApplication: Một lớp trong Spring Boot được sử dụng để khởi động và chạy ứng dụng từ phương thức main().//
 @SpringBootApplication
 public class KoiFishApplication {
 
